8e6a3216f9a2ec0c58132028529dc7d9c540b075
Matheus Almeida Santos Mendonça <matheus.mendonca@lmcorp.local>
2024-04-11 10:25:24 -0300

    fix: adicionando variaveis para auxilio na identificação do problema na função publish_flow_empty_v2

--- src/archy.py
+++ src/archy.py
@@ -264,6 +264,7 @@ class Archy:
         try:
             status, error = None, None
             flow_file_name = os.path.abspath('./inbound_call_start.yaml')
+            print(flow_file_name)
             file_flow = FileYaml(flow_file_name)
             if self.verificar_flow_prd(flow_name):
                 raise Exception(f'Fluxo: {flow_name} é utilizado nos ivrs de produção')
