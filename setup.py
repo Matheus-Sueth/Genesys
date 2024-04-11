c61e193be9862175fa1c83ba7b392541f5cfba90
Matheus Almeida Santos Mendonça <matheus.mendonca@lmcorp.local>
2024-04-11 11:08:13 -0300

    fix: adicionando no arquivo setup.py o parametro package_data_v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Genesys',
-    version='1.4.4',
+    version='1.4.5',
     packages=find_packages(),
     package_data={'': ['*.yaml']},
     author='Matheus Almeida Santos Mendonça',
