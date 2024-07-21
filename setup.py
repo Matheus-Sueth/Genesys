c32b7e56e6a52ba27aefed125099827f5e0acd79
Matheus Almeida Santos Mendonça <matheus.mendonca@lmcorp.local>
2024-07-21 16:35:37 -0300

    feat: Adicionando pre-commit ao projeto

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Genesys',
-    version='2.4.5',
+    name="Genesys",
+    version="2.5",
     packages=find_packages(),
-    package_data={'': ['*.yaml']},
-    author='Matheus Almeida Santos Mendonça',
-    author_email='matheuzengenharia@gmail.com',
-    description='Uma ferramenta para gerenciar aplicação do Archy, as APIs do Genesys Cloud',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Matheus-Sueth/Genesys.git',
-    install_requires=[
-        'python-dotenv',
-        'PyYAML',
-        'requests',
-        'setuptools'
-    ],
+    package_data={"": ["*.yaml"]},
+    author="Matheus Almeida Santos Mendonça",
+    author_email="matheuzengenharia@gmail.com",
+    description="Gerenciar aplicação do Archy, as APIs do Genesys Cloud",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matheus-Sueth/Genesys.git",
+    install_requires=["python-dotenv", "PyYAML", "requests", "setuptools"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
 )
